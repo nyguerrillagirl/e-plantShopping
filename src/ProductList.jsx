fcdb1cd93e9f0237bab43b3187d5d4b3876f4195
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-03-29 12:51:33 -0400

    forgot to update one file

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -279,7 +279,7 @@ function ProductList({ onHomeClick }) {
   };
   const handlePlantsClick = (e) => {
     e.preventDefault();
-    setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
+    //setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
     setShowCart(false); // Hide the cart when navigating to About Us
   };
 
